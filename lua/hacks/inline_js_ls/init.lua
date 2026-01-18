90476be7b1061448ebb2365bfbd3d0f584b2dfba
hyperstown <experynt@gmail.com>
2026-01-19 00:49:19 +0100

    fix lsp path

--- lua/hacks/inline_js_ls/init.lua
+++ lua/hacks/inline_js_ls/init.lua
@@ -1,17 +1,29 @@
 local M = {}
 
-function M.setup(opts) 
-  local lspconfig = require "lspconfig"
-  local configs = require "lspconfig.configs"
+local function plugin_root()
+  local source = debug.getinfo(1, "S").source
+  local file = source:sub(2)
+  return vim.fn.fnamemodify(file, ":p:h:h:h")
+end
+
+function M.setup(opts)
+  local lspconfig = require("lspconfig")
+  local configs = require("lspconfig.configs")
+
   local filetypes = opts.filetypes or { "html" }
   local settings = opts.settings or {}
 
   if not configs.inline_js_ls then
     configs.inline_js_ls = {
       default_config = {
-        cmd = { vim.fn.stdpath("data") .. "/inline-js-ls/js/src/server.js" },
+        cmd = {
+          "node",
+          plugin_root() .. "/hacks/inline_js_ls/js/src/server.js",
+        },
         filetypes = filetypes,
-        root_dir = function() return vim.loop.cwd() end,
+        root_dir = function()
+          return vim.loop.cwd()
+        end,
         settings = settings,
       },
     }
